15d3febcd89f21b451e32eb9267b12ddd41a67f0
Veronica Head <veronica.leigh.head@gmail.com>
2018-07-01 01:26:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,8 +20,9 @@ class App < Sinatra::Base
   end
   
   get '/multiply/:num1/:num2' do
-    @num1 = params[:num1]
-    @num2 = params[:num2]
-    puts num1*num2
+    @num1 = params[:num1].to_i
+    @num2 = params[:num2].to_i
+    @product = @num1*@num2
+    "#{@product}"
   end
 end
